3e66ac2334f7f75e5fbcbca071f704da130091fa
Pavel Sobolev <paveloom@mail.ru>
2021-01-01 00:15:13 +0300

    Fix users objects not being tested.

--- check.jl
+++ check.jl
@@ -9,9 +9,22 @@ const messages_dir = joinpath(@__DIR__, "messages")
 const threads_dir = joinpath(@__DIR__, "threads")
 const users_dir = joinpath(@__DIR__, "users")
 
-function test(target)
-    for dir in readdir(target, join=true)
-        for file in readdir(dir, join=true)
+function test(target; loop_dirs=true)
+    if loop_dirs
+        for dir in readdir(target, join=true)
+            for file in readdir(dir, join=true)
+                open(file, "r") do io
+                    try
+                        @test_nowarn JSON3.read(io)
+                    catch
+                        println("\n\e[1;31mWhoops! File: $(basename(file))\e[0m\n")
+                        rethrow()
+                    end
+                end
+            end
+        end
+    else
+        for file in readdir(target, join=true)
             open(file, "r") do io
                 try
                     @test_nowarn JSON3.read(io)
@@ -33,5 +46,5 @@ end
 end
 
 @testset "Users" begin
-    test(threads_dir)
+    test(users_dir, loop_dirs=false)
 end
